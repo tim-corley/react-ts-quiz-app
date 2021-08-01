230abe67876e94caa576370477dd3ed5b4324411
Tim Corley <tcorley215@gmail.com>
2021-08-01 13:30:29 -0400

    adding error handling to fetch call
    adding unit test for error response case

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,6 +3,16 @@ import testData from './testData.json';
 
 export const handlers = [
   rest.get('https://opentdb.com/api.php', async (req, res, ctx) => {
-    return res(ctx.json(testData));
+    return await res(ctx.json(testData));
+  }),
+];
+
+// https://codeburst.io/configuring-mock-service-worker-msw-c62c9d9e4a6a
+// https://mswjs.io/docs/api/response/network-error
+// https://mswjs.io/docs/recipes/mocking-error-responses
+export const networkErrorHandlers = [
+  rest.get('*', async (req, res, ctx) => {
+    // res.networkError('Oops! Something went wrong...')
+    return await res(ctx.status(404));
   }),
 ];
